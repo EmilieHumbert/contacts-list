cb26e2a733c04a19b1750440d80976fc2a503ad8
emilie <emi.m.humbert@googlemail.com>
2020-06-18 19:08:05 +0100

    retrieving users data from a database

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import React from "react";
 import ContactCard from "./ContactCard";
+import { contacts } from "./ContactDetails";
+
 import "./styles.css";
 
 const App = () => {
   return (
-    <div>
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Jenny Han"
-        email="jenny.han@notreal.com"
-        age={25}
-      />
-      ; ;
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Jason Long"
-        email="jason.long@notreal.com"
-        age={45}
-      />
-      ;
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Peter Pan"
-        email="peter.pan@neverland.com"
-        age={100}
-      />
-      ;
-    </div>
+    <>
+      {contacts.map(contact => (
+        <ContactCard
+          avatar="https://via.placeholder.com/150"
+          name={contact.name}
+          email={contact.email}
+          age={contact.age}
+          key={contact.email}
+        />
+      ))}
+    </>
   );
 };
 
