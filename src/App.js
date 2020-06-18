d7e1c94b7299e0d6bb01e8bfb946e209f7ec83db
emilie <emi.m.humbert@googlemail.com>
2020-06-18 20:10:27 +0100

    pulling data from an API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React from "react";
-import ContactCard from "./ContactCard";
-import { contacts } from "./ContactDetails";
+import Contacts from "./Contacts";
 
 import "./styles.css";
 
-const App = () => {
-  return (
-    <>
-      {contacts.map(contact => (
-        <ContactCard
-          avatar="https://via.placeholder.com/150"
-          name={contact.name}
-          email={contact.email}
-          age={contact.age}
-          key={contact.email}
-        />
-      ))}
-    </>
-  );
-};
+const App = () => <Contacts />;
 
 export default App;
--- src/ContactDetails.js
+++
@@ -1,5 +0,0 @@
-export const contacts = [
-  { name: "Jenny Han", email: "jenny.han@notreal.com", age: 25 },
-  { name: "Jason Long", email: "jason.long@notreal.com", age: 45 },
-  { name: "Peter Pan", email: "peter.pan@neverland.com", age: 100 },
-];
